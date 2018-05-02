2cf4162c481dc0d757fa524b4a064235706f13e1
Ihar Hrachyshka <ihrachys@redhat.com>
2018-05-02 11:52:05 -0700

    Deploy kubevirt role on ws too

--- _ws/run.sh
+++ _ws/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook --ask-become-pass -i ws.inventory ../_ansible/{nested,libvirt,docker,godev,dev}.yml ihar.yml
+ansible-playbook --ask-become-pass -i ws.inventory ../_ansible/{nested,libvirt,docker,godev,dev,kubevirt}.yml ihar.yml
