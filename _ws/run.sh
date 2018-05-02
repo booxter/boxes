b1c3ef9f476bd142791445ff0a45004c5fdb9368
Ihar Hrachyshka <ihrachys@redhat.com>
2018-05-02 10:37:56 -0700

    Reuse existing ansible roles for ws deployment

--- _ws/run.sh
+++ _ws/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook --ask-become-pass -i ws.inventory ws.yml ihar.yml
+ansible-playbook --ask-become-pass -i ws.inventory ../_ansible/{nested,libvirt,docker,dev}.yml ihar.yml
