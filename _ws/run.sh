94ce0b8852bb7d16265c967118229d8bc25d65b9
Ihar Hrachyshka <ihrachys@redhat.com>
2018-05-02 11:26:30 -0700

    Install godev role on ws
    I will need it to develop kubevirt from bare metal to save a layer of
    virtualization for proper hardware virtualization on the last level of
    kubevirt environment (k8s driven vms).

--- _ws/run.sh
+++ _ws/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook --ask-become-pass -i ws.inventory ../_ansible/{nested,libvirt,docker,dev}.yml ihar.yml
+ansible-playbook --ask-become-pass -i ws.inventory ../_ansible/{nested,libvirt,docker,godev,dev}.yml ihar.yml
